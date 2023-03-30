fc6a80ba2f8799b1b027ff52a7d0e6740a44bc7e
misayo <1243755514@qq.com>
2023-03-30 16:17:34 +0800

    增加日志部分

---
+++ core/logrus.go
@@ -0,0 +1,82 @@
+package core
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"gvd_project/global"
+	"os"
+	"path"
+)
+
+const (
+	red    = 31
+	yellow = 33
+	blue   = 36
+	gray   = 37
+)
+
+type LogFormatter struct {
+}
+
+func (l *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var levelColor int
+	switch entry.Level {
+	case logrus.DebugLevel, logrus.TraceLevel:
+		levelColor = gray
+	case logrus.WarnLevel:
+		levelColor = yellow
+	case logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel:
+		levelColor = red
+	default:
+		levelColor = blue
+	}
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+	log := global.Config.Logger
+	timeStamp := entry.Time.Format("2006-01-02 15:04:05")
+	if entry.HasCaller() {
+		//自定义文件路径
+		funcVal := entry.Caller.Function
+		fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
+		//自定义日志格式
+		fmt.Fprintf(b, "%s [%s] \x1b[%dm[%s]\x1b[0m %s %s %s\n",
+			log.Prefix, timeStamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
+	} else {
+		fmt.Fprintf(b, "%s [%s] \x1b[%dm[%s]\x1b[0m %s\n",
+			log.Prefix, timeStamp, levelColor, entry.Level, entry.Message)
+	}
+	return b.Bytes(), nil
+}
+
+func InitLogger() *logrus.Logger {
+	// 创建一个日志对象
+	mLog := logrus.New()                                //新建一个实例
+	mLog.SetOutput(os.Stdout)                           //设置输出类型
+	mLog.SetReportCaller(global.Config.Logger.ShowLine) //开启返回函数名和行号
+	mLog.SetFormatter(&LogFormatter{})                  //设置自己自定义的日志Formatter
+	//mLog.SetLevel(logrus.DebugLevel)   //设置最低级level
+	level, err := logrus.ParseLevel(global.Config.Logger.Level)
+	if err != nil {
+		level = logrus.InfoLevel
+	}
+	mLog.SetLevel(level)
+	InitDefaultLogger()
+	return mLog
+}
+
+func InitDefaultLogger() {
+	// 设置全局日志
+	logrus.SetOutput(os.Stdout)
+	logrus.SetReportCaller(global.Config.Logger.ShowLine) //开启返回函数名和行号
+	logrus.SetFormatter(&LogFormatter{})                  //设置自己自定义的日志Formatter
+	level, err := logrus.ParseLevel(global.Config.Logger.Level)
+	if err != nil {
+		level = logrus.InfoLevel
+	}
+	logrus.SetLevel(level)
+}
--- main.go
+++ main.go
@@ -8,8 +8,16 @@ import (
 
 // 服务入口
 func main() {
-	core.CoreConf() //读取配置文件
+	//读取配置文件
+	core.CoreConf()
 	fmt.Println(global.Config)
+	//设置日志格式
+	global.Log = core.InitLogger()
+	global.Log.Warnln("测试警告日志")
+	global.Log.Errorln("测试错误日志")
+	global.Log.Debugln("测试debug日志")
+	global.Log.Println("测试日志")
+	//连接数据库
 	global.DB = core.InitGorm()
 	fmt.Println(global.DB)
 }
